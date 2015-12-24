f3f057e786513dbe0a3471f7c32222e640da19c3
Jonathan Widawski <jonathan.widawski@gmail.com>
2015-12-24 17:43:00 +0100

    Replace LocalStorageService for CameraService (q1)

---
+++ app/scripts/services/CameraService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Places.service:CameraService
+ * @description
+ * # CameraService
+ */
+angular.module('Places')
+  // use 
+  .factory('Camera', ['$q', function($q) {
+
+	  
+	}]);
