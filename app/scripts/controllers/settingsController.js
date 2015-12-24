10a72dd94c2b176ae489adac18ee29a47cf2c6a9
Jonathan Widawski <jonathan.widawski@gmail.com>
2015-12-24 12:06:58 +0100

    Initial commit using yeoman ionic-gulp

---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Places.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Places')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
